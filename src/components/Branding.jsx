e52284606d9226bd7df01c8f121e279db86f18bd
Ibnu Topan <99580553+IbnuTopanAdib@users.noreply.github.com>
2025-06-10 18:18:51 +0700

    make test success

--- src/components/Branding.jsx
+++ src/components/Branding.jsx
@@ -5,7 +5,7 @@ const Branding = () => {
     <div className="md:w-1/2 text-left">
       <h1 className="text-blue-600 text-6xl font-bold mb-4">NuThread</h1>
       <p className="text-2xl text-gray-700">
-                NuThread adlah tempat untuk berdiskusi untuk semua kalangan app
+                NuThread adlah tempat untuk berdiskusi untuk semua kalangan
       </p>
     </div>
   );
