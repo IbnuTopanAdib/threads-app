5a106954ca1e66dc5dcc29586b2e761a5569118e
Ibnu Topan <99580553+IbnuTopanAdib@users.noreply.github.com>
2025-06-10 18:34:31 +0700

    benerin action

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import useInput from '../hooks/useInput';
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
 
 const LoginForm = ({ onLogin }) => {
   const [email, onEmailChange] = useInput('');
